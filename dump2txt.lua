9e37ebe30e2f7a3e5d4e1726062ce514a9c483c7
Glitch <iGlitch@users.noreply.github.com>
2015-07-03 18:07:23 -0400

    Update dump2txt.lua

--- dump2txt.lua
+++ dump2txt.lua
@@ -1,6 +1,6 @@
 function on_pause_change(name, value)
 if value == true then
-file = io.open("C:/Program Files/mpv-x86_64-20150505/np.txt", "w+")
+file = io.open("C:/Program Files/mpv/np.txt", "w+")
 file:write(mp.get_property("media-title"), "|", mp.get_property("file-size"), "|", mp.get_property("percent-pos"), "|", mp.get_property("length"), "|", mp.get_property("time-pos"))
 file:close()
 end
@@ -10,7 +10,7 @@ mp.observe_property("pause", "bool", on_pause_change)
 --local msg = require 'mp.msg'
 --local utils = require 'mp.utils'
 function dump_info()
-file = io.open("C:/Program Files/mpv-x86_64-20150505/np.txt", "w+")
+file = io.open("C:/Program Files/mpv/np.txt", "w+")
 file:write(mp.get_property("media-title"), "|", "|", mp.get_property("file-size"), "|", mp.get_property("percent-pos"), "|", mp.get_property("length"), "|", mp.get_property("time-pos"))
 file:close()
 end
